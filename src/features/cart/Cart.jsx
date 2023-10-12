3f3999fa166e51bfd058b5d15a89cda3fe980623
Fethi Efe Okyar <106201658+feo23dev@users.noreply.github.com>
2023-10-12 23:35:14 +0300

    Update Cart.jsx

--- src/features/cart/Cart.jsx
+++ src/features/cart/Cart.jsx
@@ -43,7 +43,7 @@ function Cart() {
   if (!cart.length) return <EmptyCart></EmptyCart>;
   return (
     <div className="px-4 py-3">
-      <LinkButton to="/menu">&larr; Back to menu</LinkButton>
+      <LinkButton to="/menu">&larr; Back to menuuu</LinkButton>
 
       <h2 className="mt-7 text-xl font-semibold">Your cart, {username}</h2>
 
